7b82dac2c59fdf53518fa60cb6beee4b836110b6
Rennie Bevineau <rsbevineau@gmail.com>
2023-09-05 14:45:27 -0600

    initial skeleton change node port

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 //Middleware: Add middleware for parsing JSON requests
 app.use(express.json());
