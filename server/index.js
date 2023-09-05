18d13b356641d9d7b5542e7de3dbd49a5fcb1bf7
Rennie Bevineau <rsbevineau@gmail.com>
2023-09-05 14:02:32 -0600

    initial skeleton full stack project with sass and post.css

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//Middleware: Add middleware for parsing JSON requests
+app.use(express.json());
+
+//API Routes
+app.get('/api/books',(req, res)=>{
+    //Implement your route logic here
+    res.json({message: 'API endpoint for books'})
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
