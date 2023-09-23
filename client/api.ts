cb598403e4b5dfb266332054f969800f44f4def6
Rennie Bevineau <rsbevineau@gmail.com>
2023-09-23 15:50:05 -0600

    add google books api call

---
+++ client/api.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+const API_ENDPOINT = process.env.NEXT_PUBLIC_Books_API_ENDPOINT;
+import { BookResponse } from "./types/api/books";
+async function fetchData() {
+  try {
+    const response: BookResponse = await axios.get(API_ENDPOINT+"history");
+    return response.data.items;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+}
+
+export { fetchData };
