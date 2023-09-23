0aba5edd1ffef3c3958d84e7b1e6489816a132d9
Rennie Bevineau <rsbevineau@gmail.com>
2023-09-23 16:15:36 -0600

    add page content

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,69 @@
-import Image from 'next/image'
-import styles from './page.module.scss'
+"use client"
+import { useEffect, useState } from 'react';
+import { fetchData } from "../api";
+import {filterAndSortGenres, filterAndSortYears} from '../helpers'
+import { Item } from '@/types/api/books';
+import ContentControls from '@/components/ContentControls/ContentControls';
+import Content from '@/components/Content/Content';
+import styles from '@/app/page.module.scss';
+
+
 
 export default function Home() {
+  const [data, setData] = useState<Item[]>([]);
+  const [genres, setGenres] = useState<string[]>([]);
+  const [years, setYears] = useState<string[]>([]);
+  const [checkedGenres, setCheckedGenres] = useState([]);
+  const [checkedYears, setCheckedYears] = useState([]);
+  const [contentTypeSelected, setSelectedContentType] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetchData()
+      .then((responseData: Item[]) => {
+
+        const sortedGenres = filterAndSortGenres(responseData);
+        const sortedYears = filterAndSortYears(responseData);
+        console.log('responseData')
+        setData(responseData);
+        setGenres(sortedGenres);
+        setYears(sortedYears);
+      })
+      .catch((error) => {
+        console.error("Fetch error:", error);
+      });
+  }, []);
+
+  const filters = {
+    genres,
+    years,
+  };
+
+  const selections = {
+    checkedGenres,
+    checkedYears,
+    contentTypeSelected,
+    searchTerm,
+  };
+
+  const selectionMethods = {
+    setCheckedGenres,
+    setCheckedYears,
+    setSelectedContentType,
+    setSearchTerm,
+  };
+
   return (
-    <main className={styles.app}>
-      <h1>Main Page</h1>
-    </main>
-  )
+    <div className={styles.app}>
+      <div>
+        <ContentControls
+          allContent={data}
+          {...filters}
+          {...selections}
+          {...selectionMethods}
+        />
+        <Content allContent={data} {...selections} />
+      </div>
+    </div>
+  );
 }
